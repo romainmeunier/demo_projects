566c37fa13c1cea9a21b7401805c28b082b1394f
djrom <djrom@new-host-3.home>
2011-01-20 15:42:36 +0100

    init 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo1::Application.config.secret_token = '5383866bde90c42c5445908504e308ce90fbdf927d46c85625f27658831ae7fedbb51e3b50b85a7df6f4d29d9d707c01f9179d5173d59fdbf371e8e7b2518201'
